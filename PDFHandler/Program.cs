e6f33b6746fec1c11eee55710af8f3c8b0a76876
QHCheng <QiHang.Cheng@Yahoo.com>
2016-07-25 14:53:29 +0800

    Foxit PDF Editor SDK for .NET

--- PDFHandler/Program.cs
+++ PDFHandler/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Drawing;
-using Spire.Pdf;
-using Spire.Pdf.Graphics;
+using Foxit.PDF;
+using Foxit.PDF.Merger;
+using Foxit.PDF.PageElements;
 
 namespace PDFHandler
 {
@@ -14,29 +14,14 @@ namespace PDFHandler
     {
         static void Main(string[] args)
         {
-            PdfDocument doc = new PdfDocument();
-            doc.LoadFromFile(@"../../PDFs/sample.pdf");
-            Image image = Image.FromFile(@"../../img/background.jpg");
+            Document doc = new Document();
 
-            int width = image.Width;
-            int height = image.Height;
-            float schale = 1.0f;
-            //Size size = new Size((int)(width * schale), (int)(height * schale));
-            Size size = new Size(width, height);
-            Bitmap schaleImage = new Bitmap(image, size);
+            ImportedPage page = new ImportedPage(@"../../PDFs/sample.pdf", 1);
+            doc.Pages.Add(page);
 
-            PdfImage pdfImage = PdfImage.FromImage(schaleImage);
-            PdfPageBase page0 = doc.Pages[0];
-            //PointF position = new PointF((page0.Canvas.ClientSize.Width - schaleImage.Width) / 10, 210);
-            PointF position = new PointF(-500, -210);
-            page0.Canvas.SetTransparency(0.65f);
-            page0.Canvas.DrawImage(pdfImage, position);
-            page0.Canvas.SetTransparency(1.0f);
+            //page.Elements.Add(new BackgroundImage(@"../../img/background.jpg"));            
 
-
-            doc.SaveToFile(@"../../PDFs/result.pdf");
-            doc.Close();
-
+            doc.Draw(@"../../PDFs/result.pdf");
         }
     }
 }
